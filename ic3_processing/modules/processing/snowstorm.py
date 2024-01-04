6012380b9a00c7a2354ecb5d997fdb44e90c9bae
mhuen <mhuen@users.noreply.github.com>
2024-01-04 16:25:31 +0100

    AddSnowStormParameterDict

---
+++ ic3_processing/modules/processing/snowstorm.py
@@ -0,0 +1,43 @@
+from icecube import icetray, dataclasses
+
+
+class AddSnowStormParameterDict(icetray.I3ConditionalModule):
+
+    """Add SnowStorm Parameters as I3MapStringDouble"""
+
+    def __init__(self, ctx):
+        super(AddSnowStormParameterDict, self).__init__(ctx)
+        self.AddParameter(
+            "FrameType", "Frame type to write SnowStorm parameters to.", "M"
+        )
+        self.AddParameter(
+            "OutputKey",
+            "Key to which the SnowStorm parameters are written to",
+            "SnowstormParameterDict",
+        )
+
+    def Configure(self):
+        self._frame_type = self.GetParameter("FrameType")
+        self._output_key = self.GetParameter("OutputKey")
+
+    def Process(self):
+        # get next frame
+        frame = self.PopFrame()
+
+        # check if this is the specified frame type to which we
+        # want to write the parameters to
+        if frame.Stop.id == self._frame_type:
+            names = []
+            for name in frame["SnowstormParametrizations"]:
+                if name == "HoleIceForward_Unified":
+                    names.append("HoleIceForward_Unified_00")
+                    names.append("HoleIceForward_Unified_01")
+                else:
+                    names.append(name)
+
+            ss_param_dict = dataclasses.I3MapStringDouble()
+            for value, name in zip(frame["SnowstormParameters"], names):
+                ss_param_dict[name] = value
+            frame[self._output_key] = ss_param_dict
+
+        self.PushFrame(frame)
